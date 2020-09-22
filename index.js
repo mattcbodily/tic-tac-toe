7e1514bf0f04d3a9dcef283541317851661ddb42
mattcbodily <mattcbodily@gmail.com>
2020-09-22 15:36:49 -0600

    Number of wins per X or O now displays, game board can be reset

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let xTiles = [],
     oTiles = [],
-    xTurn = true;
+    xTurn = true
+    xScore = 0,
+    oScore = 0;
+
+const xScoreNode = document.getElementById('x-score'),
+      oScoreNode = document.getElementById('o-score'),
+      winnerDisplay = document.getElementById('winner-display'),
+      turnDisplay = document.getElementById('turn-display');
+
+xScoreNode.innerText = `X Wins: ${xScore}`;
+oScoreNode.innerText = `O Wins: ${oScore}`;
+turnDisplay.innerText = xTurn ? "X's Turn" : "Y's Turn";
+winnerDisplay.style.display = 'none';
 
 const clearBoard = () => {
     for(let i = 1; i < 10; i++){
@@ -11,15 +23,29 @@ const clearBoard = () => {
     xTiles = [];
     oTiles = [];
     xTurn = true;
+    winnerDisplay.innerText = '';
+    winnerDisplay.style.display = 'none';
+    turnDisplay.innerText = "X's Turn";
+    turnDisplay.style.display = 'initial';
 }
 
 const checkWinner = () => {
     if((xTiles.includes(1) && xTiles.includes(2) && xTiles.includes(3)) || (xTiles.includes(4) && xTiles.includes(5) && xTiles.includes(6)) || (xTiles.includes(7) && xTiles.includes(8) && xTiles.includes(9)) || (xTiles.includes(1) && xTiles.includes(4) && xTiles.includes(7)) || (xTiles.includes(2) && xTiles.includes(5) && xTiles.includes(8)) || (xTiles.includes(3) && xTiles.includes(6) && xTiles.includes(9)) || (xTiles.includes(1) && xTiles.includes(5) && xTiles.includes(9)) || (xTiles.includes(3) && xTiles.includes(5) && xTiles.includes(7))){
-        alert('X Wins')
-        clearBoard();
+        turnDisplay.innerText = '';
+        turnDisplay.style.display = 'none';
+        winnerDisplay.innerText = 'X Wins'
+        winnerDisplay.style.display = 'initial';
+        xScore++;
+        xScoreNode.innerText = `X Wins: ${xScore}`;
+        // clearBoard();
     } else if((oTiles.includes(1) && oTiles.includes(2) && oTiles.includes(3)) || (oTiles.includes(4) && oTiles.includes(5) && oTiles.includes(6)) || (oTiles.includes(7) && oTiles.includes(8) && oTiles.includes(9)) || (oTiles.includes(1) && oTiles.includes(4) && oTiles.includes(7)) || (oTiles.includes(2) && oTiles.includes(5) && oTiles.includes(8)) || (oTiles.includes(3) && oTiles.includes(6) && oTiles.includes(9)) || (oTiles.includes(1) && oTiles.includes(5) && oTiles.includes(9)) || (oTiles.includes(3) && oTiles.includes(5) && oTiles.includes(7))){
-        alert('O Wins')
-        clearBoard();
+        turnDisplay.innerText = '';
+        turnDisplay.style.display = 'none';
+        winnerDisplay.innerText = 'O Wins'
+        winnerDisplay.style.display = 'initial';
+        oScore++;
+        oScoreNode.innerText = `O Wins: ${oScore}`;
+        // clearBoard();
     }
 }
 
@@ -27,13 +53,19 @@ const selectTile = (id) => {
     const tile = document.getElementById(`${id}`);
 
     if(xTurn){
-        tile.innerText = 'X';
-        xTiles.push(id);
-        checkWinner()
+        if(!xTiles.includes(id) && !oTiles.includes(id)){
+            tile.innerText = 'X';
+            xTiles.push(id);
+            turnDisplay.innerText = "O's Turn";
+            checkWinner();
+        }
     } else {
-        tile.innerText = 'O';
-        oTiles.push(id);
-        checkWinner()
+        if(!xTiles.includes(id) && !oTiles.includes(id)){
+            tile.innerText = 'O';
+            oTiles.push(id);
+            turnDisplay.innerText = "X's Turn";
+            checkWinner();
+        }
     }
     
     xTurn = !xTurn;
