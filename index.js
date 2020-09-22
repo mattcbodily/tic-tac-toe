3dd34c61b97de295917d7048dd47308f2b8a0cc1
mattcbodily <mattcbodily@gmail.com>
2020-09-22 16:17:47 -0600

    fixing bugs

--- index.js
+++ index.js
@@ -33,16 +33,18 @@ const checkWinner = () => {
     if((xTiles.includes(1) && xTiles.includes(2) && xTiles.includes(3)) || (xTiles.includes(4) && xTiles.includes(5) && xTiles.includes(6)) || (xTiles.includes(7) && xTiles.includes(8) && xTiles.includes(9)) || (xTiles.includes(1) && xTiles.includes(4) && xTiles.includes(7)) || (xTiles.includes(2) && xTiles.includes(5) && xTiles.includes(8)) || (xTiles.includes(3) && xTiles.includes(6) && xTiles.includes(9)) || (xTiles.includes(1) && xTiles.includes(5) && xTiles.includes(9)) || (xTiles.includes(3) && xTiles.includes(5) && xTiles.includes(7))){
         turnDisplay.innerText = '';
         turnDisplay.style.display = 'none';
-        winnerDisplay.innerText = 'X Wins'
-        winnerDisplay.style.display = 'initial';
+        // winnerDisplay.innerText = 'X Wins'
+        // winnerDisplay.style.display = 'initial';
+        alert('X Wins');
         xScore++;
         xScoreNode.innerText = `X Wins: ${xScore}`;
         // clearBoard();
     } else if((oTiles.includes(1) && oTiles.includes(2) && oTiles.includes(3)) || (oTiles.includes(4) && oTiles.includes(5) && oTiles.includes(6)) || (oTiles.includes(7) && oTiles.includes(8) && oTiles.includes(9)) || (oTiles.includes(1) && oTiles.includes(4) && oTiles.includes(7)) || (oTiles.includes(2) && oTiles.includes(5) && oTiles.includes(8)) || (oTiles.includes(3) && oTiles.includes(6) && oTiles.includes(9)) || (oTiles.includes(1) && oTiles.includes(5) && oTiles.includes(9)) || (oTiles.includes(3) && oTiles.includes(5) && oTiles.includes(7))){
         turnDisplay.innerText = '';
         turnDisplay.style.display = 'none';
-        winnerDisplay.innerText = 'O Wins'
-        winnerDisplay.style.display = 'initial';
+        // winnerDisplay.innerText = 'O Wins'
+        // winnerDisplay.style.display = 'initial';
+        alert('O Wins');
         oScore++;
         oScoreNode.innerText = `O Wins: ${oScore}`;
         // clearBoard();
